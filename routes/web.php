96ebf10ff3c256730ce1e9b7239c18bc984b2ad5
Rozel369 <rozelpo@gmail.com>
2023-08-20 16:57:28 +0700

    frontpages

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,22 @@ use App\Http\Controllers\Admin\UserRecoredsController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front_page.layout.master_frontpage');
 });
 
 // This route is redirect to login page when seit is load
-Route::redirect('/', '/login');
+// Route::redirect('/', '/login');
 
 Auth::routes();
+
+Route::group(['prefix' => 'frontpage'], function(){
+    Route::get('/', [App\Http\Controllers\Frontpage\FrontPageController::class, 'index'])->name('frontpage');
+});
 
 Route::group(['middleware' => 'auth'], function () {
 
     // Home route
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
     // Admin dashboard route
     // Route::get('/admin/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
